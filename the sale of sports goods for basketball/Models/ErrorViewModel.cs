f64383fcf0eb3711f0a57d3c6682604cf0076379
Marevan <jakionqw@gmail.com>
2023-04-20 18:55:41 +0300

    SecondCommit

---
+++ the sale of sports goods for basketball/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace the_sale_of_sports_goods_for_basketball.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ the sale of sports goods for basketball/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace the_sale_of_sports_goods_for_basketball.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+        public string Producer { get; set; }
+    }
+}
